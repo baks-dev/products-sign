f94adeb9ad95c32e446dabe9458869bce06631e8
kingstar <admin@baks.dev>
2024-01-30 22:40:52 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -33,12 +33,17 @@ return static function (FrameworkConfig $framework) {
         ->transport('products-sign')
         ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
         ->options(['queue_name' => 'products-sign'])
+        ->failureTransport('failed-products-sign')
         ->retryStrategy()
         ->maxRetries(3)
         ->delay(1000)
         ->maxDelay(0)
         ->multiplier(3) // увеличиваем задержку перед каждой повторной попыткой
         ->service(null)
+
     ;
+
+    $messenger->transport('failed-products-sign')
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%');
 
 };
