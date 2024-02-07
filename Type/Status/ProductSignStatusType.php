3a4d5d3b7757543110a92a832143f0a823a66398
kingstar <admin@baks.dev>
2024-02-08 02:14:12 +0300

    Baks Development

--- Type/Status/ProductSignStatusType.php
+++ Type/Status/ProductSignStatusType.php
@@ -27,15 +27,16 @@ namespace BaksDev\Products\Sign\Type\Status;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
 
-final class ProductSignStatusType extends StringType
+final class ProductSignStatusType extends Type
 {
-    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
     {
         return (string) $value;
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform): mixed
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?ProductSignStatus
     {
         return !empty($value) ? new ProductSignStatus($value) : null;
     }
@@ -48,5 +49,10 @@ final class ProductSignStatusType extends StringType
     public function requiresSQLCommentHint(AbstractPlatform $platform): bool
     {
         return true;
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
     }
 }
