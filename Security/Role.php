bddf62aaf256a329bad2c492888e631da1d4c916
kingstar <admin@baks.dev>
2025-03-16 17:01:33 +0300

    Baks Development

--- Security/Role.php
+++ Security/Role.php
@@ -60,7 +60,7 @@ final class Role implements RoleInterface, MenuAdminInterface
     }
 
     /** Метод возвращает позицию, в которую располагается ссылка в секции меню */
-    public function getSortMenu(): int
+    public static function getSortMenu(): int
     {
         return 253;
     }
