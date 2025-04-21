d12330086f58391f1779abdf0ad5b971c546c5fd
kingstar <admin@baks.dev>
2025-04-21 03:12:55 +0300

    Baks Development

---
+++ Repository/GroupProductSignsByOrder/GroupProductSignsByOrderRepository.php
@@ -0,0 +1,259 @@
+<?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Products\Sign\Repository\GroupProductSignsByOrder;
+
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
+use BaksDev\Orders\Order\Entity\Order;
+use BaksDev\Orders\Order\Type\Id\OrderUid;
+use BaksDev\Products\Category\Entity\Offers\CategoryProductOffers;
+use BaksDev\Products\Category\Entity\Offers\Variation\CategoryProductVariation;
+use BaksDev\Products\Category\Entity\Offers\Variation\Modification\CategoryProductModification;
+use BaksDev\Products\Product\Entity\Event\ProductEvent;
+use BaksDev\Products\Product\Entity\Offers\ProductOffer;
+use BaksDev\Products\Product\Entity\Offers\Variation\Modification\ProductModification;
+use BaksDev\Products\Product\Entity\Offers\Variation\ProductVariation;
+use BaksDev\Products\Product\Entity\Product;
+use BaksDev\Products\Product\Entity\Trans\ProductTrans;
+use BaksDev\Products\Sign\Entity\Code\ProductSignCode;
+use BaksDev\Products\Sign\Entity\Event\ProductSignEvent;
+use BaksDev\Products\Sign\Entity\Invariable\ProductSignInvariable;
+use BaksDev\Products\Sign\Entity\ProductSign;
+use BaksDev\Products\Sign\Type\Status\ProductSignStatus\ProductSignStatusProcess;
+use Generator;
+use InvalidArgumentException;
+
+final class GroupProductSignsByOrderRepository implements GroupProductSignsByOrderInterface
+{
+    private OrderUid|false $order = false;
+
+    public function __construct(private readonly DBALQueryBuilder $DBALQueryBuilder) {}
+
+
+    public function forOrder(Order|OrderUid|string $order): self
+    {
+        if(empty($order))
+        {
+            $this->order = false;
+            return $this;
+        }
+
+        if(is_string($order))
+        {
+            $order = new OrderUid($order);
+        }
+
+        if($order instanceof Order)
+        {
+            $order = $order->getId();
+        }
+
+        $this->order = $order;
+
+        return $this;
+    }
+
+
+    /** Метод возвращает список группы честных знаков заказа для скачивания */
+    public function findAll(): Generator|false
+    {
+        if(false === ($this->order instanceof OrderUid))
+        {
+            throw new InvalidArgumentException('Invalid Argument OrderUid');
+        }
+
+        $dbal = $this->DBALQueryBuilder
+            ->createQueryBuilder(self::class)
+            ->bindLocal();
+
+        $dbal->from(
+            ProductSignEvent::class,
+            'event'
+        );
+
+        $dbal
+            ->where('event.ord = :ord')
+            ->setParameter(
+                key: 'ord',
+                value: $this->order,
+                type: OrderUid::TYPE
+            );
+
+        $dbal
+            ->andWhere('event.status = :status')
+            ->setParameter(
+                key: 'status',
+                value: ProductSignStatusProcess::class,
+                type: OrderUid::TYPE
+            );
+
+
+        $dbal
+            ->addSelect('COUNT(*) AS counter')
+            ->addSelect('invariable.part AS sign_part')
+            ->join(
+                'event',
+                ProductSignInvariable::class,
+                'invariable',
+                'invariable.main = event.main'
+            );
+
+        $dbal
+            ->leftJoin(
+                'invariable',
+                ProductSignCode::class,
+                'code',
+                'code.main = invariable.main'
+            );
+
+
+        $dbal
+            ->join(
+                'invariable',
+                ProductSign::class,
+                'main',
+                'main.id = invariable.main'
+            );
+
+
+        // Product
+        $dbal
+            ->addSelect('product.id as product_id')
+            ->join(
+                'invariable',
+                Product::class,
+                'product',
+                'product.id = invariable.product'
+            );
+
+        $dbal->join(
+            'product',
+            ProductEvent::class,
+            'product_event',
+            'product_event.id = product.event'
+        );
+
+        $dbal
+            ->addSelect('product_trans.name as product_name')
+            ->join(
+                'product',
+                ProductTrans::class,
+                'product_trans',
+                'product_trans.event = product.event AND product_trans.local = :local'
+            );
+
+
+        /**
+         * Торговое предложение
+         */
+
+        $dbal
+            ->addSelect('product_offer.const as product_offer_const')
+            ->addSelect('product_offer.value as product_offer_value')
+            ->addSelect('product_offer.postfix as product_offer_postfix')
+            ->leftJoin(
+                'product',
+                ProductOffer::class,
+                'product_offer',
+                'product_offer.event = product.event AND product_offer.const = invariable.offer'
+            );
+
+
+        // Получаем тип торгового предложения
+        $dbal
+            ->addSelect('category_offer.reference as product_offer_reference')
+            ->leftJoin(
+                'product_offer',
+                CategoryProductOffers::class,
+                'category_offer',
+                'category_offer.id = product_offer.category_offer'
+            );
+
+        // Множественные варианты торгового предложения
+
+        $dbal
+            ->addSelect('product_variation.const as product_variation_const')
+            ->addSelect('product_variation.value as product_variation_value')
+            ->addSelect('product_variation.postfix as product_variation_postfix')
+            ->leftJoin(
+                'product_offer',
+                ProductVariation::class,
+                'product_variation',
+                'product_variation.offer = product_offer.id AND product_variation.const = invariable.variation'
+            );
+
+
+        // Получаем тип множественного варианта
+        $dbal
+            ->addSelect('category_variation.reference as product_variation_reference')
+            ->leftJoin(
+                'product_variation',
+                CategoryProductVariation::class,
+                'category_variation',
+                'category_variation.id = product_variation.category_variation'
+            );
+
+
+        // Модификация множественного варианта торгового предложения
+
+        $dbal
+            ->addSelect('product_modification.const as product_modification_const')
+            ->addSelect('product_modification.value as product_modification_value')
+            ->addSelect('product_modification.postfix as product_modification_postfix')
+            ->leftJoin(
+                'product_variation',
+                ProductModification::class,
+                'product_modification',
+                'product_modification.variation = product_variation.id AND product_modification.const = invariable.modification'
+            );
+
+
+        // Получаем тип модификации множественного варианта
+        $dbal
+            ->addSelect('category_offer_modification.reference as product_modification_reference')
+            ->leftJoin(
+                'product_modification',
+                CategoryProductModification::class,
+                'category_offer_modification',
+                'category_offer_modification.id = product_modification.category_modification'
+            );
+
+        // Артикул продукта
+        $dbal->addSelect(
+            '
+            COALESCE(
+                product_modification.article,
+                product_variation.article,
+                product_offer.article,
+                product_info.article
+            ) AS product_article'
+        );
+
+
+        $dbal->allGroupByExclude();
+
+        return $dbal->fetchAllHydrate(GroupProductSignsByOrderResult::class);
+    }
+}
---
+++ Repository/GroupProductSignsByOrder/GroupProductSignsByOrderResult.php
@@ -0,0 +1,161 @@
+<?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Products\Sign\Repository\GroupProductSignsByOrder;
+
+use BaksDev\Products\Product\Type\Id\ProductUid;
+use BaksDev\Products\Product\Type\Offers\ConstId\ProductOfferConst;
+use BaksDev\Products\Product\Type\Offers\Variation\ConstId\ProductVariationConst;
+use BaksDev\Products\Product\Type\Offers\Variation\Modification\ConstId\ProductModificationConst;
+use BaksDev\Products\Sign\Type\Id\ProductSignUid;
+
+final readonly class GroupProductSignsByOrderResult
+{
+
+    public function __construct(
+        private int $counter, // " => 100
+        private string $sign_part, // " => "019643c2-9025-7050-b731-95ce5a4cbc77"
+
+        private string $product_id, // " => "0194d10e-d18b-7ee9-9bbb-0ba83582342c"
+        private string $product_name, // " => "Футболка короткий рукав"
+
+        private ?string $product_offer_const, // " => "0194d10e-ce65-7dd2-97a2-0608f2683361"
+        private ?string $product_offer_value, // " => "FFFFFF"
+        private ?string $product_offer_reference, // " => "color_type"
+        private ?string $product_offer_postfix, // " => "color_type"
+
+        private ?string $product_variation_const, // " => "0194d10e-cf53-77e1-90dc-8342dd45b543"
+        private ?string $product_variation_value, // " => "3XL"
+        private ?string $product_variation_reference, // " => "size_clothing_type"
+        private ?string $product_variation_postfix, // " => "size_clothing_type"
+
+        private ?string $product_modification_const, // " => null
+        private ?string $product_modification_value, // " => null
+        private ?string $product_modification_reference, // " => null
+        private ?string $product_modification_postfix, // " => null
+
+        private string $product_article, // " => "T-WHITE-3XL"
+    ) {}
+
+    public function getCounter(): int
+    {
+        return $this->counter;
+    }
+
+    public function getSignPart(): ProductSignUid
+    {
+        return new ProductSignUid($this->sign_part);
+    }
+
+    public function getProductId(): ProductUid
+    {
+        return new ProductUid($this->product_id);
+    }
+
+
+    public function getProductName(): string
+    {
+        return $this->product_name;
+    }
+
+    public function getProductArticle(): string
+    {
+        return $this->product_article;
+    }
+
+    /**
+     * Offer
+     */
+
+    public function getProductOfferConst(): ?ProductOfferConst
+    {
+        return $this->product_offer_const ? new ProductOfferConst($this->product_offer_const) : null;
+    }
+
+    public function getProductOfferValue(): ?string
+    {
+        return $this->product_offer_value ?: null;
+    }
+
+    public function getProductOfferReference(): ?string
+    {
+        return $this->product_offer_reference ?: null;
+    }
+
+    public function getProductOfferPostfix(): ?string
+    {
+        return $this->product_offer_postfix;
+    }
+
+
+    /**
+     * Variation
+     */
+
+    public function getProductVariationConst(): ?ProductVariationConst
+    {
+        return $this->product_variation_const ? new ProductVariationConst($this->product_variation_const) : null;
+    }
+
+    public function getProductVariationValue(): ?string
+    {
+        return $this->product_variation_value ?: null;
+    }
+
+    public function getProductVariationReference(): ?string
+    {
+        return $this->product_variation_reference ?: null;
+    }
+
+    public function getProductVariationPostfix(): ?string
+    {
+        return $this->product_variation_postfix;
+    }
+
+
+    /**
+     * Modification
+     */
+
+    public function getProductModificationConst(): ?ProductModificationConst
+    {
+        return $this->product_modification_const ? new ProductModificationConst($this->product_modification_const) : null;
+    }
+
+    public function getProductModificationValue(): ?string
+    {
+        return $this->product_modification_value ?: null;
+    }
+
+    public function getProductModificationReference(): ?string
+    {
+        return $this->product_modification_reference ?: null;
+    }
+
+    public function getProductModificationPostfix(): ?string
+    {
+        return $this->product_modification_postfix;
+    }
+}
