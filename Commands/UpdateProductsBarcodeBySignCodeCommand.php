f93d2b844d61962337c4140bff44d5fa1cb59395
MichelAngelo <baksdevelopmen@gmail.com>
2026-01-20 22:54:31 +0300

    BaksDevelopment

--- Commands/UpdateProductsBarcodeBySignCodeCommand.php
+++ Commands/UpdateProductsBarcodeBySignCodeCommand.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -90,7 +90,6 @@ class UpdateProductsBarcodeBySignCodeCommand extends Command
             $io->warning('Продукции для обновления не найдено');
             return Command::INVALID;
         }
-
 
         foreach($products as $ProductsIdentifierResult)
         {
