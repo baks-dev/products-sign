1aac7054dcca88b9b99fcff4e43bbb75b9ef38f1
adminap <shestopalov92@gmail.com>
2026-03-23 17:13:12 +0300

    fork: PR-23.03.2026. Скачивание Честного знака по id события

--- Controller/Admin/PdfController.php
+++ Controller/Admin/PdfController.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,7 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
+ *
  */
 
 declare(strict_types=1);
@@ -41,6 +42,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Форма загрузки Честных знаков
+ */
 #[AsController]
 #[RoleSecurity('ROLE_PRODUCT_SIGN_NEW')]
 final class PdfController extends AbstractController
